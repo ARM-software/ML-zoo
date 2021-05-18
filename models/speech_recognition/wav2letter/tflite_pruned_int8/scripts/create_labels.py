1a92aa08c0de49a7304e0a7f3f59df6f4fd33ac8
Tom Allsop <tom.allsop@arm.com>
2021-05-18 13:44:20 +0100

    Added Wav2letter Pruned INT8

---
+++ models/speech_recognition/wav2letter/tflite_pruned_int8/scripts/create_labels.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2021 Arm Limited or its affiliates. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the License); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an AS IS BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+def get_label_dict():
+    alphabet = "abcdefghijklmnopqrstuvwxyz' @"
+    return [c for c in alphabet]
+
+if __name__ == "__main__":
+    labels = get_label_dict()
+
+    with open("labelmappings.txt", "w") as f:
+        for l in labels:
+            f.write('{}\n'.format(l))
